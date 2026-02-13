1b233808cfa716599aba6ce4f71d89c7d6309f10
CahyaAji <cahya.akk@gmail.com>
2026-02-13 23:52:35 +0700

    add df plot

---
+++ frontend/src/lib/component/RelativePlot.svelte
@@ -0,0 +1,17 @@
+<div class="container"></div>
+
+<style>
+    .container {
+        display: flex;
+        width: 230px;
+        height: 230px;
+        background-image: url("/src/assets/relative_circle.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-color: rgba(4, 61, 15, 0.5);
+        border-radius: 50%;
+        position: relative;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
