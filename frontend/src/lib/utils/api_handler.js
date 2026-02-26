11a6e7797e425bef9d07a771fc1750df572af3dc
CahyaAji <cahya.akk@gmail.com>
2026-02-26 12:01:38 +0700

    add get setting from mcu

--- frontend/src/lib/utils/api_handler.js
+++ frontend/src/lib/utils/api_handler.js
@@ -94,3 +94,20 @@ export const readCompass = async () => {
     return { success: false, error: error instanceof Error ? error.message : String(error) };
   }
 };
+
+export const getDFSettings = async () => {
+  try {
+    const response = await fetch(`${API_URL}/api/settings`);
+    const result = await response.json();
+
+    const filteredResult = {
+      center_freq: result.center_freq,
+      uniform_gain: result.uniform_gain,
+      station_id: result.station_id,
+    };
+
+    return filteredResult;
+  } catch (error) {
+    throw error;
+  }
+};
